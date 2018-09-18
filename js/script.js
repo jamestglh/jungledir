2f55c0f82c131fed9bd4fb56702d863fdf6344fb
James.Woodard <james@jungledisk.com>
2018-09-18 16:07:51 -0500

    added css and js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+$( document ).ready(function() {
+
+  $( document ).ready(function() {
+    $("ul.trackAccordion").accordion({
+    	collapsible: true,
+    	active: false,
+    	heightStyle: "content"
+    });
+  });
+
+
+});
